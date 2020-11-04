245e88ddcf8e30062ce721c295e5707d3c64fe74
Gustavo Julián <gjuliane@gmail.com>
2020-11-04 00:22:57 -0600

    enhances

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,42 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
-main(List<String> args) async{
-  Map<String, dynamic> query = {
-    "parameters[bid]": "34",
-    "parameters[cid]": "1",
-    "options[orderby][cid]": "ASC",
-    "options[orderby][hid]": "ASC",
-    "page": "0",
-    "pagesize": "200"
-  };
+// import 'package:dio/dio.dart';
 
-  final endpoint = 'https://e.5fc833fb.l5U4LWtsYOjwAcSEPfapjrhcynTiglGwbhDIbOs8hAY@develoment-kykemx.com/api/readed?';
+void main(List<String> args) async{
 
-  final queryParameters = Uri(queryParameters: query);
-  // final queryParameters = json.encode(query);
-  print("$endpoint${queryParameters.query}");
-  final response = await http.get("$endpoint${queryParameters.query}", headers: {"Content-Type": "application/json"});
+  final Map payload = {
+    'page': '0',
+    'pagesize': '200',
+    'parameters': {
+        'bid': '34',
+        'cid': '1'
+    },
+    'options': {
+        'orderby': {
+            'cid': 'ASC',
+            'hid': 'ASC'
+        }
+    }
+};
 
-  print(queryParameters);
+  final endpoint = 'https://e.5fc833fb.l5U4LWtsYOjwAcSEPfapjrhcynTiglGwbhDIbOs8hAY@develoment-kykemx.com/api/readed/custom-index';
+
+
+  // https://e.5fc833fb.l5U4LWtsYOjwAcSEPfapjrhcynTiglGwbhDIbOs8hAY@develoment-kykemx.com/api/readed?parameters[bid]=34&parameters[cid]=1&options[orderby][cid]=ASC&options[orderby][hid]=ASC&page=0&pagesize=200
+
+  final response = await http.post(
+    endpoint,
+    body: json.encode(payload),
+    headers: <String, String> { 'Content-Type': 'application/json' }
+  );
+
+  // print(queryParameters);
 
   if (response.statusCode == 200) {
     final data = json.decode(response.body);
     print(data);
   }
+  print(response.statusCode);
 }
