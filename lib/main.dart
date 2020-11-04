e657c622158a732441163edb278d25f23d7cdd59
Gustavo Julián <gustavo.julian@clabsa.com.mx>
2020-11-03 18:06:43 -0600

    enhances

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
-import 'package:http/http.dart';
-main(List<String> args) {
+import 'package:http/http.dart' as http;
+main(List<String> args) async{
   Map<String, dynamic> query = {
     "parameters[bid]": "34",
     "parameters[cid]": "1",
@@ -11,7 +11,17 @@ main(List<String> args) {
     "pagesize": "200"
   };
 
+  final endpoint = 'https://e.5fc833fb.l5U4LWtsYOjwAcSEPfapjrhcynTiglGwbhDIbOs8hAY@develoment-kykemx.com/api/readed?';
+
   final queryParameters = Uri(queryParameters: query);
   // final queryParameters = json.encode(query);
+  print("$endpoint${queryParameters.query}");
+  final response = await http.get("$endpoint${queryParameters.query}", headers: {"Content-Type": "application/json"});
+
   print(queryParameters);
+
+  if (response.statusCode == 200) {
+    final data = json.decode(response.body);
+    print(data);
+  }
 }
